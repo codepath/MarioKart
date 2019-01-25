5ce7f27936efeecb527ff2f6f6b9241d8e86a3a5
chieger <charlie@codepath.com>
2019-01-25 15:55:35 -0800

    Initial Commit

---
+++ Mario Cart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mario Cart
+//
+//  Created by Charles Hieger on 1/25/19.
+//  Copyright © 2019 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+
+}
+
